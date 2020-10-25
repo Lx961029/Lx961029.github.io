e7f90eb70c1cdc4ee19f0e2a355b9513ee051f24
Lx961029 <49628009+Lx961029@users.noreply.github.com>
2020-10-25 21:32:45 +0800

    Add files via upload

---
+++ web-desktop/src/settings.f3369.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/game.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "72bb7",
+        main: "a46ef"
+    }
+};
